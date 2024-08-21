112adde2baf34307fbe52601e527c22e6139a5a3
lex1937 <90953442+lex1937@users.noreply.github.com>
2024-08-21 15:14:06 +0300

    Add files via upload

---
+++ module1.Variables.py
@@ -0,0 +1,6 @@
+hw_fin = 12 # homework_completed
+t_for_hw_fin = 1.5 # times_for_homework_completed, (90 минут)
+course_name = "Python"
+t_for_one_hw = (90 // 12) # time_for_one_homework
+print(course_name, hw_fin, t_for_hw_fin, t_for_one_hw)
+# print(course_name, str(hw_fin) + "hours", str(t_for_hw_fin) + "hour", str(t_for_one_hw) + "min")
