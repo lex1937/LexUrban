34dc46757947ab2930336b121b11eaa8f71baff3
lex1937 <90953442+lex1937@users.noreply.github.com>
2024-10-17 21:58:04 +0300

    Add files via upload

---
+++ module2.ConditionIfElse.py
@@ -0,0 +1,10 @@
+first = int(input())
+second = int(input())
+third = int(input())
+
+if first==second==third:
+    print(3)
+elif first==second!=third or first==third!=second:
+    print(2)
+else:
+    print(0)
