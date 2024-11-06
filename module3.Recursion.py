3af17841dbed2c22dd194a2e91fc1e492d2b14c6
lex1937 <90953442+lex1937@users.noreply.github.com>
2024-11-06 21:36:46 +0300

    Update module3.Recursion.py

--- module3.Recursion.py
+++ module3.Recursion.py
@@ -1,6 +1,7 @@
-
 def get_multiplied_digits(number):
     str_number = str(number)
+    if str_number[-1] == '0':
+        str_number = str_number[:-1]
     first = int(str_number[0])
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
