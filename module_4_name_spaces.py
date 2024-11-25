32a6a1b1f2f33119d3464c59e148ae600e07cf8e
lex1937 <90953442+lex1937@users.noreply.github.com>
2024-11-25 14:29:18 +0300

    Add files via upload
    Урок по пространству имен

---
+++ module_4_name_spaces.py
@@ -0,0 +1,8 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    return inner_function()
+
+
+test_function()
+inner_function()  # Работать не будет, т.к. правило видимости имен запрещает это делать
